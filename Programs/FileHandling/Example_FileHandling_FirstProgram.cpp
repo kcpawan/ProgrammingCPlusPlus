1ab05915d60b7ac6d686785be698a25237adc34f
Pawan KC <kcpawan@gmail.com>
2026-08-03 03:18:34 +0545

    filehandling Program

---
+++ Programs/FileHandling/Example_FileHandling_FirstProgram.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ 
+int main() {
+    ofstream fout("marks.txt");         // 8.2.1 open via constructor
+    if (!fout) {
+        cout << "Could not open file for writing." << endl;
+        return 1;
+    }
+    fout << "Aakriti 78" << endl;
+    fout << "Bishal 85" << endl;
+    fout.close();
+ 
+    ifstream fin;
+    fin.open("marks.txt", ios::in);       // 8.2.2 open via open() with a mode
+    string name;
+    int score;
+    while (fin >> name >> score) {
+        cout << name << " scored " << score << endl;
+    }
+    fin.close();
+    return 0;
+}
